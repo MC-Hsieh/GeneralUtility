ef7c862ab99821b3a9daea01cc1be3cad2a0e1b5
Jhsie <Jhsie@dunetek.com.tw>
2024-01-02 09:13:01 +0800

    MOD: 1.轉移Token核心元件

---
+++ Net/DNS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneralUtility.Net
+{
+    public class DNS
+    {
+        public static string GetHostNameByIP(string strIP)
+        {
+            IPHostEntry clsHostEntry = Dns.GetHostEntry(strIP);
+            return clsHostEntry.HostName;
+        }
+
+        public static string GetIPByHostName(string strHostName)
+        {
+            IPAddress[] clsIPAddresss = Dns.GetHostAddresses(strHostName);
+            foreach (IPAddress clsAddr in clsIPAddresss) 
+            {
+                if (clsAddr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    return clsAddr.ToString();
+            }
+            return "127.0.0.1";
+        }
+    }
+}
